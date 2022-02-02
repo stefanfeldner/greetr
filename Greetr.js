4c0944807d8a6bb65f015bcfedfe8ca7c41e123a
stefanfeldner <stefanfeldner@gmail.com>
2022-02-02 19:51:00 +0100

    Adding functionality, rendering HTML on Button click

--- Greetr.js
+++ Greetr.js
@@ -1,44 +1,56 @@
 // create save enviroment using IIFE
 (function (global, $) {
+  // 'new' an object
   let Greetr = function (firstName, lastName, language) {
     return new Greetr.init(firstName, lastName, language);
   };
 
+  // hidden within the scope of the IIFE and never directly accessible
   let supportedLangs = ['en', 'es'];
 
+  // informal greetings
   let greetings = {
     en: 'Hello',
     es: 'Hola',
   };
 
+  // formal greeting
   let formalGreetings = {
     en: 'Greetings',
     es: 'Saludos',
   };
 
+  // logger messages
   let logMessages = {
     en: 'Logged in',
     es: 'Inicio sesion',
   };
 
+  // prototype with all available methods for the Greetr function
   Greetr.prototype = {
+    // 'this' refers to the calling object at execution time
     fullName: function () {
       return this.firstName + ' ' + this.lastName;
     },
     validate: function () {
+      // check that is a valid language
+      // references the externally inaccessible 'supportedLangs' within the closure
       if (supportedLangs.indexOf(this.language) === -1) {
         throw 'Invaild language';
       }
     },
+    // retrieve messages from object by referring to properties using [] syntax
     greeting: function () {
       return greetings[this.language] + ' ' + this.firstName + '!';
     },
     formalGreeting: function () {
       return formalGreetings[this.language] + ', ' + this.fullName();
     },
+    // chainable methods return their own containing object
     greet: function (formal) {
       let msg;
 
+      // if undefined or null it will be coerced to 'false'
       if (formal) {
         msg = this.formalGreeting();
       } else {
@@ -49,6 +61,8 @@
         console.log(msg);
       }
 
+      // 'this' refers to the calling object at execution time
+      // makes the method chainable
       return this;
     },
     log: function () {
@@ -57,21 +71,40 @@
       }
       return this;
     },
-    setLang: function(lang) {
+    setLang: function (lang) {
       this.language = lang;
       this.validate();
+      // make chainable
       return this;
-    }
+    },
+    HTMLGreeting: function (selector, formal) {
+      if (!$) throw 'jQuery not loaded';
+      if (!selector) throw 'Missing jQuery selector';
+      let msg;
+      if (formal) {
+        msg = this.formalGreeting();
+      } else {
+        msg = this.greeting();
+      }
+      // inject the message in the chosen place in the DOM
+      $(selector).html(msg);
+      // make chainable
+      return this;
+    },
   };
 
+  // the actual object is created here, allowing us to 'new' an object without calling 'new'
   Greetr.init = function (firstName, lastName, language) {
     let self = this;
     (self.firstName = firstName || ''),
       (self.lastName = lastName || ''),
-      (self.language = language || 'en');
+      (self.language = language || 'en'),
+      self.validate();
   };
 
+  // trick borrowed from jQuery so we don't have to use the 'new' keyword
   Greetr.init.prototype = Greetr.prototype;
 
+  // attach our Greetr to the global object, and provide a shorthand '$G' for ease our poor fingers
   global.Greetr = global.G$ = Greetr;
 })(window, jQuery);
--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-let response = G$('Stefan', 'Feldner', 'en');
-console.log(response.greet());
+// let's use our object on the click of the login button
+$('#login').on('click', () => {
+  let login = G$('Stefan', 'Feldner');
+  // hide the login button
+  $('#logindiv').hide();
+  // set the language with the form data and render to html
+  login.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+});
